1d86190f9c6b5f0b8b9041120a555d4557c48b57
jessy-johannesburg@1 <jessysree11@gmail.com>
2023-11-09 11:54:37 +0200

    Third commit

--- src/test/java/StepDefinitions/StepDefinitions.java
+++ src/test/java/StepDefinitions/StepDefinitions.java
@@ -4,33 +4,39 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import cucumber.api.java.After;
 
 
 import java.util.concurrent.TimeUnit;
 
-public class StepDefinitions  {
+public class StepDefinitions {
     public static WebDriver driver;
 //    private static LoginPage loginPage;
 
-//    public StepDefinitions() {
+    //    public StepDefinitions() {
 //        loginPage = new LoginPage(driver);
 //    }
-@Before
-public void launchBrowser(){
-    System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.manage().window().maximize();
-}
+    @Before
+    public void launchBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
     @Given("^User is on the login page$")
     public void userIsOnTheLoginPage() {
         driver.get("https://www.facebook.com/");
-        System.out.println("Lauched browser and entered URL");
+        System.out.println("Launched browser and entered URL");
     }
 
     @When("^User captures email and password \"([^\"]*)\",\"([^\"]*)\"$")
@@ -40,10 +46,6 @@ public void launchBrowser(){
         System.out.println("Entered email");
         driver.findElement(By.id("pass")).sendKeys("Sriraju.5");
         System.out.println("Entered password");
-
-
-
-
     }
 
     @Then("^User logged in successfully$")
@@ -53,18 +55,23 @@ public void launchBrowser(){
 
     }
 
-//    @Then("^User selects Electronic option$")
-//    public void UserSelectsElectronicOption() throws InterruptedException {
-//        driver.findElement(By.linkText("Electronics")).click();
-//        System.out.println("selected electronics");
-//        Thread.sleep(3000);
-//    }
+    @Then("^User selects Allow button$")
+    public void UserSelectsAllowButton() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.findElement(By.id("pass")).sendKeys("Sriraju.5");
+        driver.findElement(By.name("login")).click();
+        System.out.println("Clicked on login button");
+        Thread.sleep(5000);
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+
+    }
 
 
-@After
+    @After
 
     public void closeBrowser() {
         driver.quit();
-    System.out.println("Browser closed");
+        System.out.println("Browser closed");
     }
 }
